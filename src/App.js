8207d68dce51dee081317f528dea8761e7d3f1eb
Shifath Salam <shifath.salam.216@my.csun.edu>
2020-04-20 00:31:23 -0700

    created grid items for cards component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
 
+import { Cards, Chart, CountryOptions } from "./components";
+import styles from "./App.module.css";
+
+import { fetchData } from "./api";
+
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
   render() {
+    const { data } = this.state;
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryOptions />
+        <Chart />
       </div>
     );
   }
