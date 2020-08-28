fc92836163bc6392b818ba4a71d061234c813f6d
Shifath Salam <shifath.salam.216@my.csun.edu>
2020-08-28 02:08:24 -0700

    fixed table height to allow scrolling

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -5,19 +5,21 @@ import numeral from "numeral";
 function Table({ countries }) {
   let id = 1;
   return (
-    <table className={styles.table}>
-      <tbody>
-        {countries.map((country) => (
-          <tr key={id}>
-            <td>{id++}</td>
-            <td>{country.country}</td>
-            <td>
-              <strong>{numeral(country.cases).format("0,0")}</strong>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className={styles.table}>
+      <table style={{ width: "100%" }}>
+        <tbody>
+          {countries.map((country) => (
+            <tr key={id}>
+              <td>{id++}</td>
+              <td>{country.country}</td>
+              <td>
+                <strong>{numeral(country.cases).format("0,0")}</strong>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
